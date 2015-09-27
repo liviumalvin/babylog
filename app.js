4b5ce923778e0f5b5557c1481681827c7c25dd4f
Cornita Liviu <cornita.liviu@yahoo.com>
2015-09-27 20:45:03 +0300

    Added lost app.js file

--- app.js
+++ app.js
@@ -5,6 +5,23 @@
     //Tasker
     App = require("./tasker.js");
 
-
+    //Events
     Events = require("events");
+
+
+    //Share-able resources
+    App.shareResource('winston', require("winston"));
+    App.shareResource('config', require("./config/app.json"));
+
+    App.shareResource("events", Events);
+    App.shareResource("http", App.tasks.add("http.create"));
+
+
+    //Tasks
+    App.tasks.add("logger.capture");
+
+    App.tasks.run("http.routes");
+    App.tasks.run("http.create");
+
+
 }());
